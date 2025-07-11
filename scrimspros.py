1e0451264831bf36400290494369025902197180
javierlarap <javierlarapinillos@gmail.com>
2025-07-11 03:37:36 +0200

    Update scrimspros.py

--- scrimspros.py
+++ scrimspros.py
@@ -284,6 +284,7 @@ def ejecutar_scraping_en_bucle():
 if __name__ == "__main__":
     clonar_repositorio()
     ejecutar_scraping_en_bucle()
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+
 
 
